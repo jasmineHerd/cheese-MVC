df234cb6206398138ad626602bb2c08fc43b9532
Jasmine Herd <jthm66@mail.missouri.edu>
2018-06-16 21:19:16 -0500

    Hello+Query Parameter

--- code/hello-spring/src/main/java/com/example/hellospring/Controllers/HelloController.java
+++ code/hello-spring/src/main/java/com/example/hellospring/Controllers/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.xml.ws.RequestWrapper;
 
 @Controller
@@ -12,8 +13,10 @@ public class HelloController {
 
     @RequestMapping(value = "")
     @ResponseBody
-    public String index(){
-        return "Hello World";
+    public String index(HttpServletRequest request){
+
+        String name = request.getParameter("name");
+        return "Hello" + name;
     }
     @RequestMapping(value = "goodbye")
     @ResponseBody
