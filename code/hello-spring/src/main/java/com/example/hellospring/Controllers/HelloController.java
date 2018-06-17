84e2e699125200acbf25ca144842801be24d0858
Jasmine Herd <jthm66@mail.missouri.edu>
2018-06-16 22:08:25 -0500

    Data transfer by URL Segment

--- code/hello-spring/src/main/java/com/example/hellospring/Controllers/HelloController.java
+++ code/hello-spring/src/main/java/com/example/hellospring/Controllers/HelloController.java
@@ -1,10 +1,7 @@
 package com.example.hellospring.Controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.xml.ws.RequestWrapper;
@@ -37,6 +34,14 @@ public class HelloController {
     public String helloPost(HttpServletRequest request){
         String name = request.getParameter("name");
         return "Hello " + name;
+    }
+
+
+    @RequestMapping(value = "hello/{name}")
+    @ResponseBody
+    public String helloURLSegment(@PathVariable String name){
+        return "Hello " + name;
+
     }
 
     @RequestMapping(value = "goodbye")
