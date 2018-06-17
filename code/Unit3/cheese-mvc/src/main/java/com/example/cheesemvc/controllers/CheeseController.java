25dcf498aaeeb2831ffae8e233f66752c28e10ee
Jasmine Herd <jthm66@mail.missouri.edu>
2018-06-17 11:16:01 -0500

    Passing attribute from controller to view thymeleaf syntax

--- code/Unit3/cheese-mvc/src/main/java/com/example/cheesemvc/controllers/CheeseController.java
+++ code/Unit3/cheese-mvc/src/main/java/com/example/cheesemvc/controllers/CheeseController.java
@@ -1,45 +1,20 @@
 package com.example.cheesemvc.controllers;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 
 @Controller
-//add base requestmapping value = /cheese
-@RequestMapping("cheese")
+@RequestMapping(value = "cheese")
 public class CheeseController {
-    static HashMap<String,String>cheeses = new HashMap<>();
-    //REQUEST PATH: /cheese
-    @RequestMapping(value = "")
-    //Get rid of annotation
-    // @Requestbody
-    public String index(Model model){
-        //pass arraylist object cheeses
-        model.addAttribute("cheeses",cheeses);
-        //return "file name"
-        model.addAttribute("title","My Cheeses");
+    @RequestMapping(value ="" )
+    public String index(Model model) {
+         model.addAttribute("title","My Cheeses");
         return "cheese/index";
     }
-    @RequestMapping(value = "add", method=RequestMethod.GET)
-    public String displayAddCheeseForm(Model model){
-        model.addAttribute("title","Add Cheese");
-        return "cheese/add";
-
-    }
-    @RequestMapping(value = "add",method = RequestMethod.POST)
-    //RequestParam name MUST MATCH name of form info is coming from
-    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription){
-        cheeses.put(cheeseName,cheeseDescription);
-        // Redirect to /cheese
-        //default redirect to top Controller RequestMapping
-        return "redirect: ";
-    }
 }
+
+
 
 
 
