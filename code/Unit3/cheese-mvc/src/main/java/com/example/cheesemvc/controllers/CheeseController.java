ca29fd641294332dcea3f72af9188d3046f3dc8a
Jasmine Herd <jthm66@mail.missouri.edu>
2018-06-18 09:36:45 -0500

    add items to page.No database yet

--- code/Unit3/cheese-mvc/src/main/java/com/example/cheesemvc/controllers/CheeseController.java
+++ code/Unit3/cheese-mvc/src/main/java/com/example/cheesemvc/controllers/CheeseController.java
@@ -2,15 +2,36 @@ package com.example.cheesemvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 
 @Controller
 @RequestMapping(value = "cheese")
 public class CheeseController {
+
+    static ArrayList<String> cheeses = new ArrayList<>();
+
     @RequestMapping(value ="" )
     public String index(Model model) {
-         model.addAttribute("title","My Cheeses");
+
+        model.addAttribute("cheeses",cheeses);
+        model.addAttribute("title","My Cheeses");
         return "cheese/index";
+    }
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String desplayAddCheeseForm(Model model){
+        model.addAttribute("title","Add Cheese");
+        return "cheese/add";
+
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddCheeseForm(@RequestParam String cheeseName){
+        cheeses.add(cheeseName);
+        return "redirect:";
     }
 }
 
