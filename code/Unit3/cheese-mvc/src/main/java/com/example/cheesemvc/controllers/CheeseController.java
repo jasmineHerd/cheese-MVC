1b834df17a00c2d78c3dfec7972538114595163a
Jasmine Herd <jthm66@mail.missouri.edu>
2018-06-18 19:24:20 -0500

    Object oriented refactoring

--- code/Unit3/cheese-mvc/src/main/java/com/example/cheesemvc/controllers/CheeseController.java
+++ code/Unit3/cheese-mvc/src/main/java/com/example/cheesemvc/controllers/CheeseController.java
@@ -1,4 +1,6 @@
 package com.example.cheesemvc.controllers;
+
+import com.example.cheesemvc.models.Cheese;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,14 +8,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-
 
 @Controller
 @RequestMapping(value = "cheese")
 public class CheeseController {
 
-    static HashMap<String,String> cheeses = new HashMap<>();
+    static ArrayList<Cheese> cheeses = new ArrayList<>();
 
     @RequestMapping(value ="" )
     public String index(Model model) {
@@ -24,7 +24,7 @@ public class CheeseController {
     }
     @RequestMapping(value = "remove", method = RequestMethod.GET)
     public String displayRemoveCheeseForm(Model model) {
-        model.addAttribute("cheeses", cheeses.keySet());
+        model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "Remove Cheese");
         return "cheese/remove";
     }
@@ -48,9 +48,12 @@ public class CheeseController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName,String cheeseDescription){
-        cheeses.put(cheeseName,cheeseDescription);
-        return "redirect:";
+    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription){
+            Cheese newCheese = new Cheese(cheeseName,cheeseDescription);
+            cheeses.add(newCheese);
+            return "redirect:";
+        }
+
     }
 
 
---
+++ code/Unit3/cheese-mvc/src/main/java/com/example/cheesemvc/models/Cheese.java
@@ -0,0 +1,33 @@
+package com.example.cheesemvc.models;
+
+public class Cheese {
+
+    private String name;
+    private String decription;
+
+    public Cheese(String name, String decription) {
+        this.name = name;
+        this.decription = decription;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDecription() {
+        return decription;
+    }
+
+    public void setDecription(String decription) {
+        this.decription = decription;
+    }
+
+
+
+
+
+}
